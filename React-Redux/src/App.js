ba04e34d00cb518182f359e617e03923e4f2bcb6
Alfan Azizi <76636713+alfanazizi@users.noreply.github.com>
2024-02-12 16:44:27 +0700

    add logo txt

--- React-Redux/src/App.js
+++ React-Redux/src/App.js
@@ -1,38 +1,24 @@
-
-// // App.js
-// import React from 'react';
-// import {Provider} from 'react-redux';
-// import store from './store';
-// import CounterComponent from './CounterComponent';
-
-
-// const App = () => {
-// return (
-//     <Provider store={store}>
-//     <CounterComponent />
-//     </Provider>
-//     );
-// };
-// export default App;
-
-
-// App.js
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import notesReducers from './reducers/notesReducers'; 
 import NoteForm from './components/NoteForm';
 import NoteList from './components/NoteList'; 
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image, Text } from 'react-native';
 
-const store = createStore (notesReducers);
+const store = createStore(notesReducers);
 
 const App = () => {
     return (
         <Provider store={store}>
             <View style={styles.container}>
-                <NoteForm style={styles.input} />
-                <NoteList style={styles.list} />
+                <View style={styles.bannerContainer}>
+                    <Text style={styles.bannerText}>List Data Notes Apps</Text>
+                </View>
+                <View style={styles.center}>
+                    <NoteForm style={styles.input} />
+                    <NoteList style={styles.list} />
+                </View>
             </View>
         </Provider>
     );
@@ -40,50 +26,45 @@ const App = () => {
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      marginTop: 150,
-      color: "#00007B"
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 150,
+        color: "#00007B"
+    },
+    bannerContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    logo: {
+        width: 50, // Sesuaikan dengan ukuran logo Anda
+        height: 50, // Sesuaikan dengan ukuran logo Anda
+        marginRight: 10,
+    },
+    bannerText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        padding: 20,
+        marginTop: 40,
     },
     input: {
-      width: 1250,
-      height: 40,
-      borderColor: "#EB455F",
-      borderWidth: 2,
-      paddingHorizontal: 10,
-      borderRadius: 24,
-      marginBottom: 10,
+        width: 1250,
+        height: 40,
+        borderColor: "#EB455F",
+        borderWidth: 2,
+        paddingHorizontal: 10,
+        borderRadius: 24,
+        marginBottom: 10,
     },
-    // // button: {
-    // //   backgroundColor: "#EB455F",
-    // //   padding: 10,
-    // //   margin: 10,
-    // //   borderRadius: 24,
-    // //   shadowColor: "black",
-    // //   shadowOffset: { width: 0, height: 2 },
-    // //   shadowRadius: 6,
-    // //   shadowOpacity: 0.26,
-    // // },
-    // buttonText: {
-    //   color: "white",
-    //   fontSize: 16,
-    //   fontWeight: "500",
-    //   paddingHorizontal: 12,
-    //   textAlign: "center",
-    // },
-    // bottonGroup: {
-    //   padding: 10,
-    //   paddingVertical: 20,
-    // },
     center: {
-      width: '50%',
-      alignItems: 'center',
+        width: '50%',
+        alignItems: 'center',
     },
     list: {
-      flex: 1,
-      width: '100%',
+        flex: 1,
+        width: '100%',
     },
-  });
+});
 
 export default App;
