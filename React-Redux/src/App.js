c593fe30286dd1336c1c859718f69542c2d64126
taqiyatuzzahra <arrawi@arras-mac.local>
2024-02-12 10:58:45 +0700

    app js ya

--- React-Redux/src/App.js
+++ React-Redux/src/App.js
@@ -16,3 +16,26 @@
 // export default App;
 
 
+// App.js
+import React from 'react';
+import Provider } from 'react-redux';
+import { createStore } from 'redux';
+import notesReducer from './reducers/notesReducer'; 
+import NoteForm from './components/NoteForm';
+import NoteList from './components/NoteList'; 
+import { View } from 'react-native';
+
+const store createStore (notesReducer);
+
+const App = () => {
+    return (
+        <Provider store={store}>
+            <View>
+                <NoteForm />
+                <NoteList />
+            </View>
+        </Provider>
+    );
+};
+
+export default App;
