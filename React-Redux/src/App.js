3a8d28dd02959303f496254c0887489658c0426a
Alfan Azizi <76636713+alfanazizi@users.noreply.github.com>
2024-02-12 11:26:42 +0700

    alfan

--- React-Redux/src/App.js
+++ React-Redux/src/App.js
@@ -1,41 +1,32 @@
-
-// // App.js
-// import React from 'react';
-// import {Provider} from 'react-redux';
-// import store from './store';
-// import CounterComponent from './CounterComponent';
-
-
-// const App = () => {
-// return (
-//     <Provider store={store}>
-//     <CounterComponent />
-//     </Provider>
-//     );
-// };
-// export default App;
-
-
-// App.js
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import notesReducers from './reducers/notesReducers'; 
 import NoteForm from './components/NoteForm';
 import NoteList from './components/NoteList'; 
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 const store = createStore (notesReducers);
 
 const App = () => {
     return (
         <Provider store={store}>
-            <View>
+            <View style={styles.container}>
                 <NoteForm />
                 <NoteList />
             </View>
         </Provider>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20, // Penambahan padding
+        marginTop: 40, // Penambahan margin top
+    },
+});
 
 export default App;
--- React-Redux/src/components/NoteForm.js
+++ React-Redux/src/components/NoteForm.js
@@ -1,31 +1,51 @@
-// components/NoteForm.js
 import React, { useState } from 'react';
-import {useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { addNote } from '../actions/noteActions';
-import { View, TextInput, Button } from 'react-native';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
 
 const NoteForm = () => {
-	const [noteText, setNoteText]=useState('');
-	const dispatch = useDispatch();
+    const [noteText, setNoteText] = useState('');
+    const dispatch = useDispatch();
 
-	const handleAddNote = () => {
-		if (noteText.trim() !== '') {
-			dispatch(addNote (noteText)); 
-			setNoteText(' ');
-		}
-	};
+    const handleAddNote = () => {
+        if (noteText.trim() !== '') {
+            dispatch(addNote(noteText));
+            setNoteText('');
+        }
+    };
 
-
-	return (
-		<View>
-			<TextInput
-				placeholder="Type your note here" 
-				value={noteText}
-				onChangeText={(text) => setNoteText(text)}
-				/>
-				<Button title="Add Note" onPress={handleAddNote} /> 
-		</View>
-	);
+    return (
+        <View style={styles.container}>
+            <View style={styles.formContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Type your note here"
+                    value={noteText}
+                    onChangeText={(text) => setNoteText(text)}
+                />
+                <Button title="Add Note" onPress={handleAddNote} />
+            </View>
+        </View>
+    );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    formContainer: {
+        width: '80%',
+    },
+    input: {
+        width: '100%',
+        padding: 10,
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: 'lightgray',
+        borderRadius: 5,
+    },
+});
 
 export default NoteForm;
