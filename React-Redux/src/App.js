27defcf5cc8a0aee25891b17fef3a40a8859c82e
Alfan Azizi <76636713+alfanazizi@users.noreply.github.com>
2024-02-12 14:21:56 +0700

    in line center

--- React-Redux/src/App.js
+++ React-Redux/src/App.js
@@ -23,19 +23,58 @@ import { createStore } from 'redux';
 import notesReducers from './reducers/notesReducers'; 
 import NoteForm from './components/NoteForm';
 import NoteList from './components/NoteList'; 
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 const store = createStore (notesReducers);
 
 const App = () => {
     return (
         <Provider store={store}>
-            <View>
+            <View style={styles.container}>
                 <NoteForm />
                 <NoteList />
             </View>
         </Provider>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 250,
+    },
+    input: {
+      width: 250,
+      height: 40,
+      borderColor: "#EB455F",
+      borderWidth: 2,
+      paddingHorizontal: 10,
+      borderRadius: 24,
+      margin: 4,
+    },
+    button: {
+      backgroundColor: "#EB455F",
+      padding: 10,
+      margin: 10,
+      borderRadius: 24,
+      shadowColor: "black",
+      shadowOffset: { width: 0, height: 2 },
+      shadowRadius: 6,
+      shadowOpacity: 0.26,
+    },
+    buttonText: {
+      color: "white",
+      fontSize: 16,
+      fontWeight: "500",
+      paddingHorizontal: 12,
+      textAlign: "center",
+    },
+    bottonGroup: {
+      padding: 10,
+      paddingVertical: 20,
+    },
+  });
 
 export default App;
