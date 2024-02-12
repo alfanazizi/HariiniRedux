1691eda0716ad32c4f4062ea5310e6322c968db5
Alfan Azizi <76636713+alfanazizi@users.noreply.github.com>
2024-02-12 14:16:14 +0700

    add color in button

--- React-Redux/src/components/NoteForm.js
+++ React-Redux/src/components/NoteForm.js
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addNote } from "../actions/noteActions";
-import { View, TextInput, Button } from "react-native";
+import { View, TextInput, Button, StyleSheet, TouchableOpacity, Text } from "react-native";
 
 const NoteForm = () => {
   const [noteText, setNoteText] = useState("");
@@ -26,9 +26,50 @@ const NoteForm = () => {
         value={noteText}
         onChangeText={(text) => setNoteText(text)}
       />
-      <Button title="Add Note" onPress={handleAddNote} />
+      <TouchableOpacity style={styles.button} onPress={handleAddNote}>
+        <Text style={styles.buttonText}>Add Note</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 220,
+    },
+    input: {
+      width: 250,
+      height: 40,
+      borderColor: "#EB455F",
+      borderWidth: 2,
+      paddingHorizontal: 10,
+      borderRadius: 24,
+      margin: 4,
+    },
+    button: {
+      backgroundColor: "#EB855F",
+      padding: 10,
+      margin: 10,
+      borderRadius: 24,
+      shadowColor: "black",
+      shadowOffset: { width: 0, height: 2 },
+      shadowRadius: 6,
+      shadowOpacity: 0.26,
+    },
+    buttonText: {
+      color: "white",
+      fontSize: 16,
+      fontWeight: "500",
+      paddingHorizontal: 12,
+      textAlign: "center",
+    },
+    bottonGroup: {
+      padding: 10,
+      paddingVertical: 20,
+    },
+  });
 
 export default NoteForm;
